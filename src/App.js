2dcbfecf1b3aa268ab95477fc2def80ce02ac949
jakegodsall <jake.edward.godsall@gmail.com>
2023-01-02 10:58:17 +0000

    added dummy data object to pass as props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React from 'react';
 
+import Header from './components/Layout/Header';
+import MainDashboard from './components/Layout/MainDashboard/MainDashboard';
+import TodayDashboard from './components/Layout/TodayDashboard/TodayDashboard';
+
 import styles from './App.module.css';
 
+const DUMMY_DATA = {
+    facebook: {
+        handle: '@nathanf',
+        followers: 1987,
+        dailyData: {
+            dailyFollowers: 12,
+            dailyViews: 87,
+            dailyLikes: 52,
+        },
+    },
+    twitter: {
+        handle: '@nathanf',
+        followers: 1044,
+        dailyData: {
+            dailyFollowers: 99,
+            dailyRetweets: 117,
+            dailyLikes: 507,
+        },
+    },
+    instagram: {
+        handle: '@realnathanf',
+        followers: 11234,
+        dailyData: {
+            dailyFollowers: 1099,
+            dailyLikes: 5462,
+            dailyViews: 52343,
+        },
+    },
+    youtube: {
+        handle: 'Nathan F.',
+        followers: 8239,
+        dailyData: {
+            dailyFollowers: -144,
+            dailyLikes: 107,
+            dailyViews: 1407,
+        },
+    },
+};
+
 const App = () => {
+
+    const totalFollowers = 
+    
     return (
         <div className={styles.container}>
-            <h1 className={styles.heading}>Hello React</h1>
+            <Header></Header>
+            <MainDashboard></MainDashboard>
+            <TodayDashboard></TodayDashboard>
         </div>
     );
 };
