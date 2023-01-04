4de990efa524778aa242204c4ee442020e4c9ce4
jakegodsall <jake.edward.godsall@gmail.com>
2023-01-04 11:34:53 +0000

    added total followers to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,12 +7,14 @@ import styles from './Header.module.css';
 const Header = (props) => {
     return (
         <header className={styles.header}>
-            <h1>Social Media Dashboard</h1>
-
-            <div>
-                <h3>Dark Mode</h3>
-                <Switch />
+            <div className={styles.headerTopRow}>
+                <h1>Social Media Dashboard</h1>
+                <div>
+                    <h3>Dark Mode</h3>
+                    <Switch />
+                </div>
             </div>
+            <p>{'Total Followers: ' + props.totalFollowers.toLocaleString('en-US')}</p>
         </header>
     );
 };
